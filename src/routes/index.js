179a189f0bb11cfa02ec7ee87921efc62f50132b
Luan Lucas <71357632+lldol10@users.noreply.github.com>
2023-07-30 21:01:03 -0300

    detalhe na rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const pratosRouter = require("./pratos.routes")
 const tagsRouter = require("./tags.routes")
 const sessionRouter = require("./session.routes")
 
+
 const routes = Router()
 
 routes.use("/users", usersRouter)
 routes.use("/sessions", sessionRouter)
 routes.use("/prato", pratosRouter)
 routes.use("/tags", tagsRouter)
+
 
 module.exports = routes
